6768592e39fb368e3207925a946ba95bf32819e1
VPolikushin <vipolikushin@gmail.com>
2023-04-01 10:58:47 +0300

    add image clickListener

--- app/src/main/java/com/example/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/example/playlistmaker/MainActivity.kt
@@ -2,11 +2,20 @@ package com.example.playlistmaker
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageView
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val image = findViewById<ImageView>(R.id.poster)
+
+        image.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Pressed image", Toast.LENGTH_SHORT).show()
+        }
 
     }
 }

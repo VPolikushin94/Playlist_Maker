9868ba716f7976866d974c0b8e8eb49bb5ce2c8a
VPolikushin <vipolikushin@gmail.com>
2023-04-25 09:50:55 +0300

    add search screen

--- app/src/main/java/com/example/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/example/playlistmaker/SearchActivity.kt
@@ -1,11 +1,63 @@
 package com.example.playlistmaker
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
 
 class SearchActivity : AppCompatActivity() {
+
+    private lateinit var etSearch: EditText
+    private lateinit var buttonClear: ImageButton
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_search)
+
+        etSearch = findViewById(R.id.et_search)
+        buttonClear = findViewById(R.id.button_clear)
+
+        addTextWatcher()
+
+        buttonClear.setOnClickListener {
+            hideKeyboard()
+            etSearch.setText("")
+        }
+    }
+
+    private fun addTextWatcher() {
+        val textWatcher = object : TextWatcher {
+            override fun beforeTextChanged(p0: CharSequence?, p1: Int, p2: Int, p3: Int) {
+            }
+
+            override fun onTextChanged(s: CharSequence?, p1: Int, p2: Int, p3: Int) {
+                buttonClear.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(p0: Editable?) {
+            }
+        }
+
+        etSearch.addTextChangedListener(textWatcher)
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    private fun hideKeyboard() {
+        val keyboard = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        keyboard.hideSoftInputFromWindow(etSearch.windowToken, 0)
+        etSearch.clearFocus()
     }
 }
